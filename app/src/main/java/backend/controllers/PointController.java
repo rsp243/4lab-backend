eef1e6d0171be44a9442f6c1d066b55849d163f8
s.reshetov <s.reshetov@yadro.com>
2023-12-17 15:05:43 +0000

    fix: fix import error

--- app/src/main/java/backend/controllers/PointController.java
+++ app/src/main/java/backend/controllers/PointController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import backend.model.Points;
 import backend.DTO.PointsCreatedDTO;
 import backend.DTO.PointsDTO;
 import backend.DTO.PointsDeletedDTO;
